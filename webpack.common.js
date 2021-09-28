f85ada1a9eb49cd365b2a081cc879b12fc6be8b5
Peteru <lorddubbs@gmail.com>
2021-09-28 05:57:02 +0100

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -100,7 +100,7 @@ module.exports = {
       alias: {
         "@": path.resolve(__dirname, "src"),
       },
-      extensions: ["*", ".js", ".json"]
+      extensions: ["*", ".js", ".jsx", ".json"]
     },
     optimization: {
       moduleIds: "deterministic",
